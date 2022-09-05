c526a0c40e746fb9f63c7ff1233ecef744b13698
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-05 21:42:25 +0300

    type tuple

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ print(systems)
 
 plane: complex = (-0.5) ** 0.5
 print(plane)
+
+cortege = tuple('hello, world!')
+print(cortege[1:3])
+
+name, age, company, position = ("Tom", 37, "Google", "software developer")
+print(name)         # Tom
+print(age)          # 37
+print(position)     # software developer
+print(company)     # Google
+
+tom = ("Tom", 37, "Google", "software developer")
+
+# получем подкортеж с 1 по 3 элемента (не включая)
+print(tom[1:3])  # (37, "Google")
+
+# получем подкортеж с 0 по 3 элемента (не включая)
+print(tom[:3])  # ("Tom", 37, "Google")
+
+# получем подкортеж с 1 по послдений элемент
+print(tom[1:])  # (37, "Google", "software developer")
